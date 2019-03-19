46172acaf3054209f6be373d9db2074a5a475c73
Divakar Singh <divakar.singh.1105@gmail.com>
2019-03-20 00:17:47 +0530

    Updates

---
+++ precache-manifest.fcf06a5b4c9fc926862dc1aa82085424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c540dfb8fe030ab859",
+    "url": "/react-app/static/css/main.9f973766.chunk.css"
+  },
+  {
+    "revision": "84c540dfb8fe030ab859",
+    "url": "/react-app/static/js/main.11863671.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-app/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "b1c8749e28336956716b0640012336df",
+    "url": "/react-app/static/media/logo.b1c8749e.svg"
+  },
+  {
+    "revision": "3319411f392440b2636c0faed2a53623",
+    "url": "/react-app/index.html"
+  }
+];
